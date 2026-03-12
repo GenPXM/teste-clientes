721cf74a8e5ee097d31fc0140909519710ad369e
GenPXM <genilsonpeixoto65@gmail.com>
2026-03-11 21:30:50 -0400

    fix: update CORS origin to allow requests from the production URL

--- api/api-clientes/src/main.ts
+++ api/api-clientes/src/main.ts
@@ -6,7 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['https://teste-clientes-lzhv.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
